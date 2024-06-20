bbde92424b410b911c136000e2fd2433fafa3f2d
rohitas-git <rohitasbansal@icloud.com>
2024-06-20 16:53:13 +0530

    - refining the safest approach: 1st using hashmap for hurdle dist freq mapping and then 2nd using no of stumbles and no of running comparisons - coded the leading strategy

--- events/olymbits/src/main.rs
+++ events/olymbits/src/main.rs
@@ -1,5 +1,5 @@
 #![allow(dead_code)]
-use std::io;
+use std::{collections::HashMap, io};
 
 macro_rules! parse_input {
     ($x:expr, $t:ident) => {
@@ -9,25 +9,25 @@ macro_rules! parse_input {
 
 #[derive(Debug, Clone)]
 struct PlayerDetails {
-    my_pos: i32,
-    my_stun: i32,
-    my_id: i32,
+    pos: i32,
+    stun: i32,
+    id: i32,
 }
 
 impl PlayerDetails {
     fn default() -> Self {
         PlayerDetails {
-            my_pos: 0,
-            my_stun: 0,
-            my_id: 0,
+            pos: 0,
+            stun: 0,
+            id: 0,
         }
     }
 
     fn new(my_pos: i32, my_stun: i32, my_id: i32) -> Self {
         PlayerDetails {
-            my_pos,
-            my_stun,
-            my_id,
+            pos: my_pos,
+            stun: my_stun,
+            id: my_id,
         }
     }
 }
@@ -37,6 +37,7 @@ struct Game {
     track: Option<String>,
     closest_hurdle: Option<usize>,
     my_player: PlayerDetails,
+    other_players: Vec<PlayerDetails>,
 }
 
 impl Game {
@@ -45,14 +46,16 @@ impl Game {
             track: None,
             closest_hurdle: None,
             my_player: PlayerDetails::default(),
+            other_players: Vec::new(),
         }
     }
 
-    fn new(my_player: PlayerDetails) -> Self {
+    fn new(my_player: PlayerDetails, other_players: Vec<PlayerDetails>) -> Self {
         Game {
             track: None,
             closest_hurdle: None,
             my_player,
+            other_players,
         }
     }
 
@@ -96,7 +99,7 @@ impl Game {
 
     fn decide(&mut self) -> String {
         if self.track.is_some() {
-            let my_pos = self.my_player.my_pos as usize;
+            let my_pos = self.my_player.pos as usize;
 
             // find closest hurdle if unknown
             if self.closest_hurdle.is_none() {
@@ -106,26 +109,25 @@ impl Game {
             // when no hurlde then is it len of track or len of track + 1
             let closest_hurdle = self
                 .closest_hurdle
-                .map_or(self.track.as_ref().unwrap().len(), |val| val);
+                .or_else(|| Some(self.track.as_ref().unwrap().len()))
+                .unwrap();
+            // .map_or(self.track.as_ref().unwrap().len(), |val| val);
 
             let dist = closest_hurdle - my_pos;
             dbg!(dist);
-            match dist {
-                1 => {
-                    self.closest_hurdle = None;
-                    return Self::up();
-                }
-                2 => return Self::left(),
-                3 => return Self::down(),
-                _ => return Self::right(),
+
+            let to_move = Game::move_based_on_dist(dist);
+            if to_move == *"UP" {
+                self.closest_hurdle = None;
             }
+            return to_move;
         }
         Self::nothing()
     }
 
     fn find_closest_hurdle(&self) -> Option<usize> {
         let track = self.track.as_ref().unwrap();
-        let my_pos = self.my_player.my_pos as usize;
+        let my_pos = self.my_player.pos as usize;
         let ahead_track = &track[my_pos..];
         ahead_track
             .split_once('#')
@@ -134,6 +136,15 @@ impl Game {
 
     fn make_move(my_move: String) {
         println!("{my_move}");
+    }
+
+    fn move_based_on_dist(dist: usize) -> String {
+        match dist {
+            1 => Self::up(),
+            2 => Self::left(),
+            3 => Self::down(),
+            _ => Self::right(),
+        }
     }
 
     fn move_to_value(this_move: &String) -> u8 {
@@ -156,20 +167,90 @@ impl Game {
     }
 
     fn move_with_safest_approach(games: &mut Vec<Self>) -> String {
-        let mut my_moves: Vec<String> = Vec::new();
-        let mut my_move = "LEFT".to_owned();
+        let mut hurdle_freq_map: HashMap<usize, u8> = HashMap::new();
+        let mut num_stumbles = 0;
+        let mut num_running = 0;
+        let mut min_stumbling_dist = 4;
+        let mut max_running_dist = 0;
 
         for game in games {
-            let todo_move = game.decide();
-            my_moves.push(todo_move.clone());
-            if todo_move == *"UP" {
-                my_move = todo_move;
+            let closest_hurdle = game
+                .find_closest_hurdle()
+                .or_else(|| Some(game.track.as_ref().unwrap().len()))
+                .unwrap();
+            let dist = closest_hurdle - game.my_player.pos as usize;
+            let curr_move = Game::move_based_on_dist(dist);
+            let future_dist = dist - Game::move_to_value(&curr_move) as usize;
+            if future_dist == 0 {
+                num_stumbles +=1;
+                if dist < min_stumbling_dist{
+                    min_stumbling_dist = dist;
+                }
+            }else{
+                num_running +=1;
+                if dist > max_running_dist {
+                    max_running_dist = dist;
+                }
+            }
+            
+            // if let Some(val) = hurdle_freq_map.get_mut(&dist) {
+            //     *val += 1
+            // } else {
+            //     hurdle_freq_map.insert(dist, 1);
+            // }
+        }
+
+        if num_running > num_stumbles {
+            Game::move_based_on_dist(max_running_dist)
+        }else{
+            Game::move_based_on_dist(min_stumbling_dist)
+        }
+
+        // if hurdle_freq_map.contains_key(&1) {
+        //     if hurdle_freq_map.contains_key(&2) {
+        //         if hurdle_freq_map.contains_key(&3) {
+        //             Game::right()
+        //         } else {
+        //             Game::down()
+        //         }
+        //     } else {
+        //         Game::left()
+        //     }
+        // } else if hurdle_freq_map.get(&2).is_none()
+        //     || hurdle_freq_map.get(&1).unwrap() > hurdle_freq_map.get(&2).unwrap()
+        // {
+        //     Game::up()
+        // } else {
+        //     Game::left()
+        // }
+    }
+
+    fn move_with_leading_approach(games: &mut Vec<Self>) -> String {
+        let mut weakest_lead = 0;
+        let mut weakest_game = None;
+        for game in games {
+            if game.track.is_some() {
+                let mut my_lead = 0;
+                let my_p = game.my_player.pos;
+                let p2 = game.other_players[0].pos;
+                let p3 = game.other_players[1].pos;
+                let mut positions = [my_p, p2, p3];
+                positions.sort();
+                if my_p == positions[2] {
+                    my_lead = my_p - positions[1];
+                } else {
+                    my_lead = my_p - positions[2];
+                }
+
+                if game.my_player.stun == 0 && (weakest_game.is_none() || my_lead < weakest_lead) {
+                    weakest_lead = my_lead;
+                    weakest_game = Some(game.clone());
+                }
             }
         }
-        if my_move != *"UP" {
-            my_move = Game::get_safest_move(&my_moves);
-        }
-        my_move
+        weakest_game
+            .map_or(Some("DOWN".to_string()), |mut game| Some(game.decide()))
+            .unwrap()
     }
 
     fn get_safest_move(my_moves: &[String]) -> String {
@@ -207,7 +288,6 @@ fn main() {
             io::stdin().read_line(&mut input_line).unwrap();
             let score_info = input_line.trim_matches('\n').to_string();
             let inputs = score_info.split(" ").collect::<Vec<_>>();
-            assert_eq!(inputs.len(), 4);
 
             let final_score = parse_input!(inputs[0], u32);
             let nb_gold_medals = parse_input!(inputs[1], u32);
@@ -231,10 +311,6 @@ fn main() {
 
         // choose my move for all 4 games
         let mut my_games: Vec<Game> = Vec::new();
-
-        // let mut weakest_track = "".to_owned();
-        // let mut weakest_pos = -1;
-        // let mut weakest_player = PlayerDetails::default();
 
         // start each mini-game
         for _i in 0..nb_games as usize {
@@ -261,29 +337,30 @@ fn main() {
 
             let positions = [reg_0, reg_1, reg_2];
             let stuns = [reg_3, reg_4, reg_5];
+            let ids = [0, 1, 2];
 
-            let my_player = PlayerDetails {
-                my_id: player_idx,
-                my_pos: positions[player_idx as usize],
-                my_stun: stuns[player_idx as usize],
-            };
+            let mut players = vec![];
+            for _i in ids {
+                let p = PlayerDetails {
+                    id: _i,
+                    pos: positions[_i as usize],
+                    stun: stuns[_i as usize],
+                };
+                players.push(p);
+            }
+            let my_player = players.remove(player_idx as usize);
 
-            if gpu != "GAME_OVER" && my_player.my_stun == 0 {
-                // if weakest_pos == -1 || my_player.my_pos < weakest_pos{
-                //     weakest_track = gpu;
-                //     weakest_pos = my_player.my_pos;
-                //     weakest_player = my_player;
-                // }
-                let mut game = Game::new(my_player);
+            if gpu != "GAME_OVER" && my_player.stun == 0 {
+                let mut game = Game::new(my_player, players);
                 game.add_track(gpu);
                 my_games.push(game);
             }
         }
-        let safest_move = Game::move_with_safest_approach(&mut my_games);
-        Game::make_move(safest_move);
-        // let mut game = Game::new(weakest_player);
-        // game.add_track(weakest_track);
-        // Game::make_move(game.decide())
+        // let safest_move = Game::move_with_safest_approach(&mut my_games);
+        // Game::make_move(safest_move);
+
+        let leading_move = Game::move_with_leading_approach(&mut my_games);
+        Game::make_move(leading_move);
     }
 }
 
